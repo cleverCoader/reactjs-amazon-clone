ee8eaf8b0f77ade8c6a1b1364188c9a1b93e96fd
cleverCoader <adventure.developer45@gmail.com>
2020-09-24 21:10:14 +0530

    Home page banner is done

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import './Home.css';
 function Home() {
     return (
         <div className="home">
-            
+            <img className="home_image" src="https://wallpapercave.com/wp/wp7513911.jpg" alt="" />
         </div>
     )
 }
--- src/MaterialUi.js
+++ src/MaterialUi.js
@@ -6,5 +6,19 @@ import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
     {
         "name": "amazon-logo-white",
         "imgUrl":"http://pngimg.com/uploads/amazon/amazon_PNG25.png"
-    }
+    },
+    {
+        "name": "amazon-homepage-image",
+        "imgUrl":"https://wallpapercave.com/wp/wp7513911.jpg"
+    },
+    {
+        "name": "earth-surface",
+        "imgUrl": "https://wallpapercave.com/wp/wp3166648.jpg"
+    },
+    {
+        "name": "amazon-bg",
+        "imgUrl": "https://wallpapercave.com/wp/wp2957449.jpg"
+    },
+
+
 ]
